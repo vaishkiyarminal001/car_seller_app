518675cb8c2df610c1619e277843875e18eae0c7
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2023-10-03 18:01:21 +0530

    Createdt Search and pagination page

--- src/Component/Paginitation.jsx
+++ src/Component/Paginitation.jsx
@@ -1,7 +1,42 @@
+import "./Paginitation.css";
+
 export const Paginitation = () =>{
+
+    const totalPage = 10;
+    const currentPage = 2;
+
+    const pageNumber = Array.from({length: totalPage}, (_, index) => index + 1);
     return(
         <div className="paginitation">
-            paginitation
+            {currentPage > 1 && (
+                
+                <button className="page-button" onClick={() => navigateToPage(currentPage -1)}>
+                    Previous
+                </button>
+            )}
+            
+            {pageNumber.map((pageNumber) =>(
+                <span
+                key={pageNumber}
+                className={`page-number ${pageNumber === currentPage ? 'active' : ''}`}
+                onClick={() => navigateToPage(pageNumber)}
+              >
+                {pageNumber}
+              </span>
+
+
+            ))}
+
+{currentPage < totalPage && (
+        <button className="page-button" onClick={() => navigateToPage(currentPage + 1)}>
+          Next
+        </button>
+)}
         </div>
-    )
-}
+    );
+
+    function navigateToPage(pageNumber) {
+       
+        console.log(`Navigating to page ${pageNumber}`);
+      }
+};
